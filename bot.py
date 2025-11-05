e52cf6c44940df69acb4c9c2442fd1f74e56fc64
stikovich <154244325+stikovich@users.noreply.github.com>
2025-11-05 12:47:03 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,16 +14,16 @@ async def start_timer(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # Обработка команды /starttimer с указанием даты
     try:
         date_str = context.args[0]
-        target_date = datetime.strptime(date_str, '%d-%m-%Y')  # Парсим введённую дату
+        target_date = datetime.strptime(date_str, '%d.%m.%Y')  # Парсим введённую дату
     except (IndexError, ValueError):
-        await update.message.reply_text('Использование: /starttimer DD-MM-YYYY')
+        await update.message.reply_text('Использование: /starttimer DD.MM.YYYY')
         return
     
     chat_id = update.effective_chat.id
     current_time = datetime.now()
     delta = target_date - current_time
     total_seconds = max(delta.total_seconds(), 0)  # Проверяем, чтобы разница была положительной
-    initial_message = await update.message.reply_text(f'Таймер запущен до {target_date.strftime("%d.%m.%Y")}')
+    initial_message = await update.message.reply_text(f'ВОЗВРАЩЕНИЕ?: {target_date.strftime("%d.%m.%Y")}')
 
     async def edit_message():
         nonlocal total_seconds
@@ -37,13 +37,13 @@ async def start_timer(update: Update, context: ContextTypes.DEFAULT_TYPE):
             await context.bot.edit_message_text(
                 chat_id=chat_id,
                 message_id=initial_message.message_id,
-                text=f'До выбранной даты осталось: {days_left:.0f} дн., {hours_left:.0f} ч., {minutes_left:.0f} мин.'
+                text=f'Увидимся через {days_left:.0f} дн. {hours_left:.0f} ч. {minutes_left:.0f} мин.'
             )
         
         await context.bot.edit_message_text(
             chat_id=chat_id,
             message_id=initial_message.message_id,
-            text='Цель достигнута! Время истекло.'
+            text='Новый Год'
         )
 
     asyncio.create_task(edit_message())
@@ -55,4 +55,5 @@ def main() -> None:
 
 if __name__ == '__main__':
     main()
+
 
